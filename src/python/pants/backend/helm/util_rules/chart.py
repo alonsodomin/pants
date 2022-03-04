8dd150c510a30d1893130eae27e433ac94b6975d
Antonio Alonso Dominguez <alonso.domin@pm.me>
2022-03-04 10:04:43 +0100

    Use a HelmChartRequest type for the HelmChart-based rules

--- src/python/pants/backend/helm/util_rules/chart.py
+++ src/python/pants/backend/helm/util_rules/chart.py
@@ -46,8 +46,6 @@ class HelmChart:
     metadata: HelmChartMetadata
     snapshot: Snapshot
 
-    lint_strict: bool | None
-
     @property
     def path(self) -> str:
         return self.metadata.name
